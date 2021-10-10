61f9f512323050e08d0bebfcbbc1a1dae090062e
Jochen Topf <jochen@topf.org>
2021-10-10 10:21:57 +0200

    Fix buildings.lua example script
    https://github.com/openstreetmap/osm2pgsql/issues/1577

--- examples/buildings/buildings.lua
+++ examples/buildings/buildings.lua
@@ -2,9 +2,7 @@
 local buildings = osm2pgsql.define_area_table('buildings', {
     -- Define an autoincrementing id column, QGIS likes a unique id on the table
     { column = 'id', sql_type = 'serial', create_only = true },
-    -- The 'multi=false' setting tells osm2pgsql to split up MultiPolygons into
-    -- several Polygon geometries.
-    { column = 'geom', type = 'polygon', multi = false },
+    { column = 'geom', type = 'polygon' },
 })
 
 function osm2pgsql.process_way(object)
@@ -18,7 +16,9 @@ end
 function osm2pgsql.process_relation(object)
     if object.tags.type == 'multipolygon' and object.tags.building then
         buildings:add_row({
-            geom = { create = 'area' }
+            -- The 'split_at' setting tells osm2pgsql to split up MultiPolygons
+            -- into several Polygon geometries.
+            geom = { create = 'area', split_at = 'multi' }
         })
     end
 end
